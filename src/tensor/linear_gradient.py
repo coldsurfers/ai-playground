bc33ab34da02c8fc9d7a05172863146f1a7f18e9
yungblud <killi8n@gmail.com>
2024-09-08 17:07:03 +0900

    feat: added linear_gradient.py

---
+++ src/tensor/linear_gradient.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+
+x = tf.constant([1.,3.,5.,7.])
+y = tf.constant([2.,3.,4.,5.])
+w = tf.Variable(1.)
+b = tf.Variable(0.5)
+learning_rate = 0.01
+epochs = 1000
+
+def train_step(x, y):
+    with tf.GradientTape() as t:
+        y_hat = w * x + b
+        loss = (y_hat - y) ** 2
+        grads = t.gradient(loss, [w, b])
+        w.assign_sub(learning_rate * grads[0])
+        b.assign_sub(learning_rate * grads[1])
+
+for i in range(epochs):
+    for k in range(len(y)):
+        train_step(x[k], y[k])
+
+print('w: {:8.5f}   b: {:8.5f}'.format(w.numpy(), b.numpy()))
