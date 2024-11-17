a7e1c31aea758adce631fb103b8156fbde411ba3
yungblud <killi8n@gmail.com>
2024-11-17 12:40:34 +0900

    feat: :sparkles: added console log

--- src/keras/conv.py
+++ src/keras/conv.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+print("train data size: %d" %train_images.shape[0])
+print("test data size: %d" %test_images.shape[0])
+print("image size: %d X %d" %(train_images.shape[1], train_images.shape[2]))
+print("answer example: %s" %str(train_labels[:20]))
+print("train image example: \n%s" %str(train_images[1]))
