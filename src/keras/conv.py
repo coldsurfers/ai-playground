f54f41469438ff900c4575654e8ccce0ffbd1810
yungblud <killi8n@gmail.com>
2024-11-17 12:43:29 +0900

    feat: :sparkles: implemented classification

--- src/keras/conv.py
+++ src/keras/conv.py
@@ -17,3 +17,14 @@ plt.imshow(train_images[1])
 plt.colorbar()
 plt.grid(False)
 plt.show()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[1], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
