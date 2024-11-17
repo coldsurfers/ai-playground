fda45c032cc6a951b505fa7e3f6a5ebd7b9ee340
yungblud <killi8n@gmail.com>
2024-11-17 13:37:18 +0900

    feat: :sparkles: data padding and normalize

--- src/keras/lenet-5.py
+++ src/keras/lenet-5.py
@@ -8,3 +8,15 @@ import numpy as np
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+x_train = np.pad(
+    x_train,
+    ((0, 0,), (2, 2), (2, 2), (0, 0)), 'constant'
+)
+x_test = np.pad(
+    x_test,
+    ((0, 0), (2, 2), (2, 2), (0, 0)),
+    'constant'
+)
+x_train = x_train / 255
+x_test = x_test / 255
