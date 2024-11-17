67646c9efdf6710c0b655a3b6d505403d0a5259e
yungblud <killi8n@gmail.com>
2024-11-17 13:36:00 +0900

    feat: :sparkles: prepare data

--- src/keras/lenet-5.py
+++ src/keras/lenet-5.py
@@ -4,3 +4,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Layer
 from keras import backend as K
 import numpy as np
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
