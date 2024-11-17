6e33b1135923d43158a66f8561d5eb5c65e17fb9
yungblud <killi8n@gmail.com>
2024-11-17 13:41:02 +0900

    feat: :sparkles: implemented RBFLayer

--- src/keras/lenet-5.py
+++ src/keras/lenet-5.py
@@ -23,3 +23,27 @@ x_test = x_test / 255
 
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
+
+class RBFLayer(Layer):
+    def __init__(self, units, gamma, **kwargs):
+        super(RBFLayer, self).__init__(**kwargs)
+        self.units = units
+        self.gamma = K.cast_to_floatx(gamma)
+
+    def build(self, input_shape):
+        self.mu = self.add_weight(
+            name='mu',
+            shape=(int(input_shape[1]), self.units),
+            initializer='uniform',
+            trainable=True
+        )
+        super(RBFLayer, self).build(input_shape)
+
+    def call(self, inputs):
+        diff = K.expand_dims(inputs) - self.mu
+        l2 = K.sum(K.pow(diff, 2), axis=1)
+        res = K.exp(-1 * self.gamma * l2)
+        return res
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.units)
