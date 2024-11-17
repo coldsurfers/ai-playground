d3f174bf591923a3776e325568fc866a44fb51a0
yungblud <killi8n@gmail.com>
2024-11-17 13:35:05 +0900

    feat: :sparkles: import modules

---
+++ src/keras/lenet-5.py
@@ -0,0 +1,6 @@
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Layer
+from keras import backend as K
+import numpy as np
